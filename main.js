475dacc38a67afc0eb4101773c6a0cbb137fc42e
Rina <foxytealblue@gmail.com>
2026-06-04 23:01:13 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  if ((x === 0 && y === 0) || y === 0) {
+    return console.error("You cannot divide by 0.");
+  }
+  return x / y;
+}
+
+function operate(x, y, operator) {
+  let result = undefined;
+  switch (operator) {
+    case "+":
+      result = add(x, y);
+      break;
+    case "-":
+      result = subtract(x, y);
+      break;
+    case "*":
+      result = multiply(x, y);
+      break;
+    case "/":
+      result = divide(x, y);
+      break;
+    default:
+      console.error("You went and broke it, congrats.");
+  }
+  return result;
+}
